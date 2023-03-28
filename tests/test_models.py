1fd03320aff239fd6152313ba1efa61b3dd01e15
Hugo Ricketts <h.ricketts@manchester.ac.uk>
2023-03-28 15:34:41 +0100

    add parameterised tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,109 @@
 import pandas as pd
 import pandas.testing as pdt
 import datetime
+import pytest
+
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+                [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[0.0, 0.0]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        ),
+        (
+                [[1, 2], [3, 4], [5, 6]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[3.0, 4.0]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        )
+    ]
+)
+def test_daily_mean(test_data, test_index, test_columns,
+                    expected_data, expected_index, expected_columns):
+    """Test mean function works with zeros and positive integers"""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(
+        daily_mean(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+        pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
+
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+                [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[0.0, 0.0]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        ),
+        (
+                [[1, 2], [3, 4], [5, 6]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[5, 6]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        )
+    ]
+)
+def test_daily_max(test_data, test_index, test_columns,
+                   expected_data, expected_index, expected_columns):
+    """Test mean function works with zeros and positive integers"""
+    from catchment.models import daily_max
+    pdt.assert_frame_equal(
+        daily_max(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+        pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
+
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+                [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[0.0, 0.0]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        ),
+        (
+                [[1, 2], [3, 4], [5, 6]],
+                [pd.to_datetime('2000-01-01 01:00'),
+                 pd.to_datetime('2000-01-01 02:00'),
+                 pd.to_datetime('2000-01-01 03:00')],
+                ['A', 'B'],
+                [[1, 2]],
+                [datetime.date(2000, 1, 1)],
+                ['A', 'B']
+        )
+    ]
+)
+def test_daily_min(test_data, test_index, test_columns,
+                   expected_data, expected_index, expected_columns):
+    """Test mean function works with zeros and positive integers"""
+    from catchment.models import daily_min
+    pdt.assert_frame_equal(
+        daily_min(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+        pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
 
 
 def test_daily_mean_zeros():
@@ -10,18 +113,18 @@ def test_daily_mean_zeros():
     from catchment.models import daily_mean
 
     test_input = pd.DataFrame(
-                     data=[[0.0, 0.0],
-                           [0.0, 0.0],
-                           [0.0, 0.0]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
+        data=[[0.0, 0.0],
+              [0.0, 0.0],
+              [0.0, 0.0]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
     )
     test_result = pd.DataFrame(
-                     data=[[0.0, 0.0]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
+        data=[[0.0, 0.0]],
+        index=[datetime.date(2000, 1, 1)],
+        columns=['A', 'B']
     )
 
     # Need to use Pandas testing functions to compare arrays
@@ -33,18 +136,18 @@ def test_daily_mean_integers():
     from catchment.models import daily_mean
 
     test_input = pd.DataFrame(
-                     data=[[1, 2],
-                           [3, 4],
-                           [5, 6]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
+        data=[[1, 2],
+              [3, 4],
+              [5, 6]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
     )
     test_result = pd.DataFrame(
-                     data=[[3.0, 4.0]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
+        data=[[3.0, 4.0]],
+        index=[datetime.date(2000, 1, 1)],
+        columns=['A', 'B']
     )
 
     # Need to use Pandas testing functions to compare arrays
@@ -56,18 +159,18 @@ def test_daily_min_integers():
     from catchment.models import daily_min
 
     test_input = pd.DataFrame(
-                     data=[[1, 2],
-                           [3, 4],
-                           [5, 6]],
-                     index=[pd.to_datetime('2000-01-01 01:00'),
-                            pd.to_datetime('2000-01-01 02:00'),
-                            pd.to_datetime('2000-01-01 03:00')],
-                     columns=['A', 'B']
+        data=[[1, 2],
+              [3, 4],
+              [5, 6]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
     )
     test_result = pd.DataFrame(
-                     data=[[1, 2]],
-                     index=[datetime.date(2000, 1, 1)],
-                     columns=['A', 'B']
+        data=[[1, 2]],
+        index=[datetime.date(2000, 1, 1)],
+        columns=['A', 'B']
     )
 
     # Need to use Pandas testing functions to compare arrays
@@ -95,4 +198,3 @@ def test_daily_max_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_max(test_input), test_result)
-
